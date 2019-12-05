2a394a1c89e5c2fcfd94c5902c2ad6bad071b007
Anton Vogel <vogel@itq.de>
2019-12-05 17:28:10 +0100

    Refactor

--- source/DeviceTabWidget.cpp
+++ source/DeviceTabWidget.cpp
@@ -5,16 +5,16 @@ DeviceTabWidget::DeviceTabWidget(QWidget *parent) : QTabWidget(parent){
 */
 //     save function (load file for addresses) so you don't have to enter all manually again
 //     AssemblyIP 192.168.0.5:4840
-        UA_Client* uaClientAssembly = UA_Client_new(UA_ClientConfig_default);
-        UA_Client_connect(uaClientAssembly, "opc.tcp://localhost:4840");
+    UA_Client* uaClientAssembly = UA_Client_new(UA_ClientConfig_default);
+    UA_Client_connect(uaClientAssembly, "opc.tcp://localhost:4840");
 
         // SuperTrakIP 192.168.0.5:4840
-        UA_Client* uaClientST = UA_Client_new(UA_ClientConfig_default);
-        UA_Client_connect(uaClientST, "opc.tcp://localhost:4840");
+    UA_Client* uaClientST = UA_Client_new(UA_ClientConfig_default);
+    UA_Client_connect(uaClientST, "opc.tcp://localhost:4840");
 
-        // LabelingIP 192.168.0.102:4840
-        UA_Client* uaClientLabeling = UA_Client_new(UA_ClientConfig_default);
-        UA_Client_connect(uaClientLabeling, "opc.tcp://localhost:4840");
+    // LabelingIP 192.168.0.102:4840
+    UA_Client* uaClientLabeling = UA_Client_new(UA_ClientConfig_default);
+    UA_Client_connect(uaClientLabeling, "opc.tcp://localhost:4840");
 
 //        UA_Client* uaClientImageRecognition = UA_Client_new(UA_ClientConfig_default);
 //        UA_Client_connect(uaClientImageRecognition, "opc.tcp://localhost:4840");
@@ -27,29 +27,29 @@ DeviceTabWidget::DeviceTabWidget(QWidget *parent) : QTabWidget(parent){
 
         /** Module STL maps */
         // you give the modulewidget the ModuleMap as well as the Skill Map, create the DeviceWidget within the Module itself
-        std::map<char*, char*> DisplayName_NodeId_Assembly;
-        DisplayName_NodeId_Assembly["Assembly"]  = "::AsGlobalPV:gAssemblyModule.state.stateMachine.operationalState";
-        std::map<char*, char*> DisplayName_NodeId_AssemblySkills;
+    std::map<char*, char*> DisplayName_NodeId_Assembly;
+    DisplayName_NodeId_Assembly["Assembly"]  = "::AsGlobalPV:gAssemblyModule.state.stateMachine.operationalState";
+    std::map<char*, char*> DisplayName_NodeId_AssemblySkills;
         //    DisplayName_NodeId_AssemblySkills["Go Home"]  = "::AsGlobalPV:gAssemblyModule.skill.goHome.state.stateMachine.operationalState";
-        DisplayName_NodeId_AssemblySkills["Provide Cup"]  = "::AsGlobalPV:gAssemblyModule.skill.provideCup.state.stateMachine.operationalState";
-        DisplayName_NodeId_AssemblySkills["Provide Pellet"]  = "::AsGlobalPV:gAssemblyModule.skill.providePellet.state.stateMachine.operationalState";
+    DisplayName_NodeId_AssemblySkills["Provide Cup"]  = "::AsGlobalPV:gAssemblyModule.skill.provideCup.state.stateMachine.operationalState";
+    DisplayName_NodeId_AssemblySkills["Provide Pellet"]  = "::AsGlobalPV:gAssemblyModule.skill.providePellet.state.stateMachine.operationalState";
 
-        std::map<char*, char*> DisplayName_NodeId_ST;
-        DisplayName_NodeId_ST["SuperTrak"]  = "::AsGlobalPV:gSuperTrak.state.stateMachine.operationalState";
-        std::map<char*, char*> DisplayName_NodeId_STSkills;
+    std::map<char*, char*> DisplayName_NodeId_ST;
+    DisplayName_NodeId_ST["SuperTrak"]  = "::AsGlobalPV:gSuperTrak.state.stateMachine.operationalState";
+    std::map<char*, char*> DisplayName_NodeId_STSkills;
         //    DisplayName_NodeId_STSkills["Outfeed"]  = "::AsGlobalPV:gSuperTrak.skill.Outfeed.state.stateMachine.operationalState";
-        DisplayName_NodeId_STSkills["checkParking"]  = "::AsGlobalPV:gSuperTrak.skill.checkParking.state.stateMachine.operationalState";
-        DisplayName_NodeId_STSkills["moveShuttle1"]  = "::AsGlobalPV:gSuperTrak.skill.moveShuttle[1].state.stateMachine.operationalState";
-        DisplayName_NodeId_STSkills["moveShuttle2"]  = "::AsGlobalPV:gSuperTrak.skill.moveShuttle[2].state.stateMachine.operationalState";
-        DisplayName_NodeId_STSkills["moveShuttle3"]  = "::AsGlobalPV:gSuperTrak.skill.moveShuttle[3].state.stateMachine.operationalState";
-        DisplayName_NodeId_STSkills["moveShuttle4"]  = "::AsGlobalPV:gSuperTrak.skill.moveShuttle[4].state.stateMachine.operationalState";
-        DisplayName_NodeId_STSkills["moveShuttle5"]  = "::AsGlobalPV:gSuperTrak.skill.moveShuttle[5].state.stateMachine.operationalState";
-        DisplayName_NodeId_STSkills["moveShuttle6"]  = "::AsGlobalPV:gSuperTrak.skill.moveShuttle[6].state.stateMachine.operationalState";
+    DisplayName_NodeId_STSkills["checkParking"]  = "::AsGlobalPV:gSuperTrak.skill.checkParking.state.stateMachine.operationalState";
+    DisplayName_NodeId_STSkills["moveShuttle1"]  = "::AsGlobalPV:gSuperTrak.skill.moveShuttle[1].state.stateMachine.operationalState";
+    DisplayName_NodeId_STSkills["moveShuttle2"]  = "::AsGlobalPV:gSuperTrak.skill.moveShuttle[2].state.stateMachine.operationalState";
+    DisplayName_NodeId_STSkills["moveShuttle3"]  = "::AsGlobalPV:gSuperTrak.skill.moveShuttle[3].state.stateMachine.operationalState";
+    DisplayName_NodeId_STSkills["moveShuttle4"]  = "::AsGlobalPV:gSuperTrak.skill.moveShuttle[4].state.stateMachine.operationalState";
+    DisplayName_NodeId_STSkills["moveShuttle5"]  = "::AsGlobalPV:gSuperTrak.skill.moveShuttle[5].state.stateMachine.operationalState";
+    DisplayName_NodeId_STSkills["moveShuttle6"]  = "::AsGlobalPV:gSuperTrak.skill.moveShuttle[6].state.stateMachine.operationalState";
 
-        std::map<char*, char*> DisplayName_NodeId_Labeling;
-        DisplayName_NodeId_Labeling["Labeling"]  = "gLabelingModule.state.stateMachine.operationalState";
-        std::map<char*, char*> DisplayName_NodeId_LabelingSkills;
-        DisplayName_NodeId_LabelingSkills["Label"]  = "gLabelingModule.skill.label.state.stateMachine.operationalState";
+    std::map<char*, char*> DisplayName_NodeId_Labeling;
+    DisplayName_NodeId_Labeling["Labeling"]  = "gLabelingModule.state.stateMachine.operationalState";
+    std::map<char*, char*> DisplayName_NodeId_LabelingSkills;
+    DisplayName_NodeId_LabelingSkills["Label"]  = "gLabelingModule.skill.label.state.stateMachine.operationalState";
 
         // Human Assembly
 //        std::map<char*, char*> DisplayName_NodeId_HumanAssembly;
@@ -87,32 +87,33 @@ DeviceTabWidget::DeviceTabWidget(QWidget *parent) : QTabWidget(parent){
 //        DisplayName_NodeId_SeedSupplySkills["provideItemToWelding"]  = "::AsGlobalPV:opcComMitsubishi.skill.provideItemToWelding.state.stateMachine.operationalState";
 //        DisplayName_NodeId_SeedSupplySkills["releaseItemToST"]  = "::AsGlobalPV:opcComMitsubishi.skill.releaseItemToST.state.stateMachine.operationalState";
 
-        DeviceWidget *AssemblyTab = new DeviceWidget(uaClientAssembly, DisplayName_NodeId_Assembly, DisplayName_NodeId_AssemblySkills, 6, 0 , this);
-        DeviceWidget *STTab = new DeviceWidget(uaClientST, DisplayName_NodeId_ST,DisplayName_NodeId_STSkills, 6, 1, this);
-        DeviceWidget *LabelingTab = new DeviceWidget(uaClientLabeling, DisplayName_NodeId_Labeling,DisplayName_NodeId_LabelingSkills, 2,2, this);
+        // TODO: these will be members
+    DeviceWidget *AssemblyTab = new DeviceWidget(uaClientAssembly, DisplayName_NodeId_Assembly, DisplayName_NodeId_AssemblySkills, 6, 0 , this);
+    DeviceWidget *STTab = new DeviceWidget(uaClientST, DisplayName_NodeId_ST,DisplayName_NodeId_STSkills, 6, 1, this);
+    DeviceWidget *LabelingTab = new DeviceWidget(uaClientLabeling, DisplayName_NodeId_Labeling,DisplayName_NodeId_LabelingSkills, 2,2, this);
 //        DeviceWidget *HumanAssemblyTab = new DeviceWidget(uaClientHumanAssembly, DisplayName_NodeId_HumanAssemblySkills, 6, this);
 //        DeviceWidget *ImageRecognitionTab = new DeviceWidget(uaClientImageRecognition, DisplayName_NodeId_ImageRecognitionSkills, 6, this);
 //        DeviceWidget *OutfeedTab = new DeviceWidget(uaClientOutfeed, DisplayName_NodeId_OutfeedSkills, 6, this);
 //        DeviceWidget *SealingTab = new DeviceWidget(uaClientSealing, DisplayName_NodeId_SealingSkills, 6, this);
 //        DeviceWidget *SeedSupplyTab = new DeviceWidget(uaClientSeedSupply, DisplayName_NodeId_SeedSupply, DisplayName_NodeId_SeedSupplySkills, 6, 3, this);
 
-        connect(AssemblyTab, &DeviceWidget::ChangeDeviceStatus, this, &DeviceTabWidget::on_ChangeDeviceStatus);
-        connect(STTab, &DeviceWidget::ChangeDeviceStatus, this, &DeviceTabWidget::on_ChangeDeviceStatus);
-        connect(LabelingTab, &DeviceWidget::ChangeDeviceStatus, this, &DeviceTabWidget::on_ChangeDeviceStatus);
+    connect(AssemblyTab, &DeviceWidget::ChangeDeviceStatus, this, &DeviceTabWidget::on_ChangeDeviceStatus);
+    connect(STTab, &DeviceWidget::ChangeDeviceStatus, this, &DeviceTabWidget::on_ChangeDeviceStatus);
+    connect(LabelingTab, &DeviceWidget::ChangeDeviceStatus, this, &DeviceTabWidget::on_ChangeDeviceStatus);
     //    connect(SeedSupplyTab, &DeviceWidget::ChangeDeviceStatus, this, &MainWindow::on_ChangeDeviceStatus);
-        tabBar()->setStyleSheet("font-size : 24px");
-        addTab(AssemblyTab,"Assembly");
-        addTab(STTab,"SuperTrak");
-        addTab(LabelingTab, "Labeling");
+    tabBar()->setStyleSheet("font-size : 24px");
+    addTab(AssemblyTab,"Assembly");
+    addTab(STTab,"SuperTrak");
+    addTab(LabelingTab, "Labeling");
 //        addTab(SeedSupplyTab, "SeedSupply");
 //        addTab(HumanAssemblyTab, "HumanAssembly");
 //        addTab(ImageRecognitionTab, "ImageRecognition");
 //        addTab(OutfeedTab, "Outfeed");
 //        addTab(SealingTab, "Sealing");
 //        addTab(SeedSupplyTab, "Seed Supply");
-        this->setTabPosition(West);
-        m_TabStyle = new TabStyle_HorizontalText();
-        tabBar()->setStyle(m_TabStyle);
+    this->setTabPosition(West);
+    m_TabStyle = new TabStyle_HorizontalText();
+    tabBar()->setStyle(m_TabStyle);
 }
 
 void DeviceTabWidget::on_ChangeDeviceStatus(int index, QString textColour, QString tabText){
@@ -121,6 +122,7 @@ void DeviceTabWidget::on_ChangeDeviceStatus(int index, QString textColour, QStri
 }
 
 DeviceTabWidget::~DeviceTabWidget(){
+    delete m_TabStyle;
 
 }
 
