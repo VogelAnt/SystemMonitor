bc04e24c65c9e2f4b0f6826a62c234cbedb4dbea
Dat Tien Nguyen <nguyen@itq.de>
2019-12-30 14:37:03 +0100

    add build scripts for linux

---
+++ scripts/build.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+BUILD_TESTS_OPTION=OFF
+
+for i in "$@"; do
+    case $i in
+    --build-tests)
+        BUILD_TESTS_OPTION=ON
+        ;;
+    -f|--force-rebuild)
+        if [ -d "build" ]; then
+            rm -rf "build"
+        fi
+        ;;
+    *)
+        echo "Unknown argument $i"
+        exit 1
+    esac
+done
+
+if [ ! -d "build" ]; then
+    mkdir build
+fi
+
+cd build
+cmake -DSMG_BUILD_TESTS=$BUILD_TESTS_OPTION .. || {
+    echo "CMake failed."
+    exit 1
+}
+make -j$(proc)
+cd ..
--- scripts/prebuild.sh
+++ scripts/prebuild.sh
@@ -1,46 +1,85 @@
+for i in "$@"; do
+    case $i in
+    -f|--force)
+        if [ -d external ]; then
+            rm -rf external
+        fi
+        ;;
+    *)
+        echo "Unknown argument $i" && exit 1
+    esac
+done
+
 if [ ! -d external ]; then
     mkdir external
 fi
-
 cd external
 
-# opcua-server-c (reusing their structure)
-if [ ! -d "opc-ua-server-c" ]; then
-    git clone https://gitex.itq.de/smart4i_v3/mocks/opc-ua-server-c.git
-else
-    cd "opc-ua-server-c"
-    git pull
-    cd ..
+
+
+# googletest
+if [ ! -d googletest ]; then
+    curl -L -o googletest.zip https://github.com/google/googletest/archive/release-1.8.1.zip
+    unzip googletest.zip
+    mv googletest-release-1.8.1 googletest
+    rm -f googletest.zip
 fi
 
-exit # delete this line after testing is done
 
-# redistorium
-if [ ! -d redistorium ]; then
-	git clone https://gitex.itq.de/smart4i_v3/libraries/redistorium.git
-else
-    cd redistorium
-    git pull
-    cd ..
-fi
-cd redistorium
-./ci-scripts/prebuild.sh
-qmake && make
-cd ..
 
 # nlohmann's JSON library
 if [ ! -d json ]; then
-    git clone https://github.com/nlohmann/json.git
+    echo "Downloading nlohmann json..."
+	git clone https://github.com/nlohmann/json.git || {
+        echo "Could not download nlohmann json." && exit 1
+    }
 else
     cd json
-    git pull
+    echo "Updating nlohmann json..." && git pull || {
+        echo "Could not update nlohmann json." && exit 1
+    }
     cd ..
 fi
 
 
 
-# open62541 (OPC-UA)
-rm -rf open62541/
-mkdir open62541
-curl -L https://github.com/open62541/open62541/releases/download/v0.3.0/open62541-linux64.tar.gz -o open62541-v0.3.0.tar.gz
-tar -xzvf open62541-v0.3.0.tar.gz -C open62541
+# open62541
+if [ ! -d open62541 ]; then
+    echo "Downloading open62541..."
+	git clone https://github.com/open62541/open62541.git || {
+        echo "Could not download open62541." && exit 1
+    }
+else
+    cd open62541
+    echo "Updating open62541..." && git pull || {
+        echo "Could not update open62541." && exit 1
+    }
+    cd ..
+fi
+cd open62541
+if [ ! -d build ]; then
+    mkdir build
+fi
+cd build
+cmake -DBUILD_SHARED_LIBS=ON -DUA_ENABLE_AMALGAMATION=ON .. || { exit 1; }
+make || { exit 1; }
+cd ../..
+
+
+
+# redistorium
+if [ ! -d redistorium ]; then
+    echo "Downloading redistorium..."
+	git clone https://gitex.itq.de/smart4i_v3/libraries/redistorium.git || {
+        echo "Could not download redistorium." && exit 1
+    }
+else
+    cd redistorium
+    echo "Updating redistorium..." && git pull || {
+        echo "Could not update redistorium." && exit 1
+    }
+    cd ..
+fi
+cd redistorium
+./ci-scripts/build.sh || { exit 1; }
+cd ..
