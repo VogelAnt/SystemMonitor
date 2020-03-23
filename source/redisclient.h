11807fbd5a306bcd945ec09710e7f7a3ca5204d8
Anton Vogel <vogel@itq.de>
2020-03-23 20:20:19 +0100

    documentation

--- source/redisclient.h
+++ source/redisclient.h
@@ -21,7 +21,7 @@ public:
     Redistorium::Redis *m_Redis = nullptr;
 
     /**
-     * @brief RedisClient::stringify_json
+     * @brief stringify_json
      * converts JSON object to usable string for redis server
      * @param object_to_be_stringified
      * the nlohman::json object
@@ -29,21 +29,53 @@ public:
      */
     QString stringify_json(nlohmann::json);
 
+    /**
+     * @brief Subscribe
+     * Subscribes to specified channel
+     * @param eChannel
+     */
     void Subscribe(QString eChannel) {
         m_Redis->SUBSCRIBE(eChannel);
     }
-
+    /**
+     * @brief Publish
+     * Publiushes to specified channel
+     * @param eChannel
+     * @param eMessage
+     */
     void Publish(const QString &eChannel, const QString &eMessage) {
         m_Redis->PUBLISH(eChannel, eMessage);
     }
 
 signals:
+    /**
+     * @brief ReceivedJSONString
+     * Emitted upon initial get and subscription
+     */
     void ReceivedJSONString(QString);
+    /**
+     * @brief SubscriptionMessage
+     * Called to pass new messages from subscribed channel
+     * @param eChannel
+     * @param eMessage
+     */
     void SubscriptionMessage(const QString &eChannel, const QString &eMessage);
+    /**
+     * @brief ParsedJson
+     * emitted when data contained in parsed json message is not empty
+     * passes data in parsed json message to OrderWidget class
+     */
     void ParsedJson(nlohmann::json);
-    void ReceivedNewSubscription(std::optional<QString>);
+    /**
+     * @brief ReceivedNewSubscription
+     *
+     */
 
 public slots:
+    /**
+     * @brief on_ReadFromJsonString
+     * received JSON in form of QString is parsed into nlohmann json format
+     */
     void on_ReadFromJsonString(QString);
 };
 
